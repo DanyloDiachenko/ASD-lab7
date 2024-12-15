a41add0635e74f1887417d516a5b233aa1df5e84
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-12-15 23:43:29 +0200

    _

--- index.c
+++ index.c
@@ -16,21 +16,22 @@ void bubbleSort(float arr[], int arrLength) {
 int main() {
     const int columns = 6;
     const int rows = 4;
-    float resultArr[columns] = {};
-    float matrix1[rows][columns] = {
-        {10, 30, 10, 70, 65, 72},
-        {50, 72, 11, -49, -32, 18},
-        {98, -99, -12, 56, 65, 92},
-        {21, 86, 43, -45, 76, -98}
+    float startArray[24] = {
+        10, 30, 10, 70, 65, 72,
+        50, 72, 11, -49, -32, 18,
+        98, -99, -12, 56, 65, 92,
+        21, 86, 43, -45, 76, -98
     };
+    float matrix[rows][columns] = {};
+    float resultArr[columns] = {};
 
     for (int col = 0; col < columns; col++) {
         float product = 1;
         bool hasNegative = false;
 
         for (int row = 0; row < rows; row++) {
-            if (matrix1[row][col] < 0) {
-                product *= matrix1[row][col];
+            if (matrix[row][col] < 0) {
+                product *= matrix[row][col];
                 hasNegative = true;
             }
         }
